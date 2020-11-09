a4044e383d844ccc45a0ba3b78d38d2ffbce328b
Sania Madoo <saniamadoo@Sanias-MacBook-Air.local>
2020-11-09 23:06:45 +0530

    i created a platform

--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+
+
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
@@ -17,7 +19,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
-
+platform= new Ground(150,305,300,170)
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -57,6 +59,6 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+platform.display();
     bird.display();
 }
